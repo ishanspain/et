4089a7e2a3f34d81d9a90562a99b7c9ab29d1871
Ishan Sharma <ishan.spain@gmail.com>
2026-09-08 17:02:40 +0530

    feat: add request throttling middleware and logger utility
    - Implemented a request throttling middleware (`ct`) to manage request rates based on client IP.
    - Introduced a `Logger` class for logging messages with environment-based behavior.
    - Added TypeScript configuration for improved type checking and output settings.
    - Created initial files for middleware and logger with example usage.

---
+++ src/logger.ts
@@ -0,0 +1,34 @@
+type env = 'dev' | 'prod' | 'test';
+
+export class Logger {
+  constructor(private readonly env: env) {}
+
+  log(...args: unknown[]): void {
+    if (this.env === 'dev') {
+      console.log(...args);
+    }
+  }
+
+  static logger(...args: unknown[]): void {
+    console.log(...args);
+  }
+}
+
+const name = 'karan';
+
+const data = {
+    name: 'John Doe',
+}
+
+const arr = [1, 2, 3, 4, 5];
+
+// ===========instance========
+const logger = new Logger('dev');
+logger.log(`dev instance log message ${name}`, data, arr);
+
+// =========prod instance========
+const logger2 = new Logger('prod');
+logger2.log(`prod instance log message ${name}`, data, arr);
+
+// ========static========
+Logger.logger("static log message");
