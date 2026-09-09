e76e6c7813f14549e7f346ca35b3e977bd5b2d0e
Ishan Sharma <ishan.spain@gmail.com>
2026-09-09 15:50:38 +0530

    feat: add new QR module and remove unused package.json for test-two

--- test-two/test-two.ts
+++ test-two/test-two.ts
@@ -1,8 +1,1 @@
 import { Logger } from "../dist";
-import { Logger as etLogger } from "express-throtle";
-
-// import { Logger  as etLogger} from "et";
-
-
-Logger.logger("log 1")
-etLogger.Logger.logger("log 2")
