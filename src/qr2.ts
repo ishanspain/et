6c1d4b5a5d0b67f51cab2692de4d55a7aae0b880
Ishan Sharma <ishan.spain@gmail.com>
2026-09-09 16:11:35 +0530

    Remove background rectangle from AT_LOGO_SVG in qr2.ts

--- src/qr2.ts
+++ src/qr2.ts
@@ -29,7 +29,6 @@ const QrCodeWithLogoConstructor = QrCodeWithLogo as unknown as new (options: {
 // external image file is needed in the generated QR code.
 const AT_LOGO_SVG = `
   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 123 60">
-    <rect width="123" height="60" fill="#202124"/>
     <path fill="#4d7fe8" d="M14 47 28 12h11l15 35H44l-3-9H27l-4 9h-9Zm17-16h7l-4-11-3 11Zm20-19h55v9H85v26H75V21H51v-9Z"/>
   </svg>
 `;
